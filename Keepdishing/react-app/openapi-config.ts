cae4cb13c395411512584a3f97e935cab7f1e9ce
Riyaadh <riyaadh.abr@gmail.com>
2022-06-15 10:42:49 +0200

    Add NextJS and remove Vite SPA

--- Keepdishing/react-app/openapi-config.ts
+++ Keepdishing/react-app/openapi-config.ts
@@ -2,9 +2,9 @@ import type { ConfigFile } from '@rtk-query/codegen-openapi'
 
 const config: ConfigFile = {
   schemaFile: 'http://localhost:5216/swagger/v1/swagger.json',
-  apiFile: './src/store/api/baseApi.ts',
+  apiFile: './store/api/baseApi.ts',
   apiImport: 'baseApi',
-  outputFile: './src/store/api/api.ts',
+  outputFile: './store/api/api.ts',
   exportName: 'api',
   hooks: true,
 }
