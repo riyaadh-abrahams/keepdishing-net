5af0dea1be90da5a62ce9ad1a4658d2a08676235
Riyaadh <riyaadh.abr@gmail.com>
2022-06-16 23:29:03 +0200

    Pass data to the NextJS Server as it passes through our .Net application

--- Keepdishing/Program.cs
+++ Keepdishing/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using AspNetCore.Proxy;
 using dotenv.net;
 using Npgsql;
+using Serilog;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 
 #region Builder
@@ -15,6 +17,23 @@ var frontendPort = Environment.GetEnvironmentVariable("FRONTEND_PORT");
 var frontendUrl = $"http://{frontendHost}:{frontendPort}/";
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((ctx, lc) =>
+{
+    lc.WriteTo.Console();
+});
+
+/*
+ * We should only allow HTTP 1.1 during development or else we will get warnings
+ * when we proxy to the NextJS server
+ */
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    if (builder.Environment.IsDevelopment())
+    {
+        serverOptions.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http1);
+    }
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
@@ -31,27 +50,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 });
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options =>
-{
-    options.SignIn.RequireConfirmedAccount = true;
-
-}).AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
-
-builder.Services.AddSpaStaticFiles(configuration =>
-{
-    configuration.RootPath = "react-app/dist";
-});
-
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SupportNonNullableReferenceTypes();
-});
-
+builder.Services.AddSwaggerGen();
 builder.Services.AddProxies();
 
 #endregion
@@ -73,8 +76,6 @@ else
 }
 
 app.UseStaticFiles();
-app.UseSpaStaticFiles();
-
 app.UseRouting();
 
 //Add Header to API Requests so that we return 401 instead of redirecting to login page 
@@ -97,7 +98,34 @@ app.UseEndpoints(endpoints =>
     endpoints.MapControllers();
 });
 
-app.RunProxy(proxy => proxy.UseHttp(frontendUrl));
+/**
+ * Proxy Requests to the NextJS Server if no API or MVC Routes has been matched at this point
+ * Include some error handling. If the Frontend Server is not running then show an error page
+ */
+app.RunProxy(proxy => proxy.UseHttp(frontendUrl,
+    builder =>
+    {
+        builder.WithBeforeSend((c, hrm) =>
+        {
+            // Set something that is needed for the downstream endpoint.
+            hrm.Headers.Add("Username", "Test");
+            return Task.CompletedTask;
+        });
+
+        builder.WithAfterReceive((c, hrm) =>
+         {
+             hrm.Headers.Add("Username", "Test");
+             return Task.CompletedTask;
+         });
+
+        builder.WithHandleFailure(async (c, e) =>
+        {
+
+            c.Response.StatusCode = 403;
+            await c.Response.WriteAsync("Things borked.");
+        });
+    }
+));
 
 
 app.Run();
--- Keepdishing/react-app/pages/index.tsx
+++ Keepdishing/react-app/pages/index.tsx
@@ -1,13 +1,29 @@
 import { Box, Center, Heading } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
+import { InferGetServerSidePropsType } from "next";
+import Link from "next/link";
 
-const Home = () => {
+const Home = ({
+  user,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <Box>
+      {user}
       <Center>
         <Heading>Home</Heading>
+        <Link href="/test">
+          <a>Go To Test</a>
+        </Link>
       </Center>
     </Box>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  var user = context.req.headers["username"];
+  return {
+    props: { user: user },
+  };
 };
 
 export default Home;
