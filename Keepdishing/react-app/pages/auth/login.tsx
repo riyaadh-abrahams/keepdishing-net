2fd8f4f1e9c6708781f46fd25b690095e3bfb3e3
Riyaadh <riyaadh.abr@gmail.com>
2022-06-26 20:43:50 +0200

    Login styles

--- Keepdishing/react-app/pages/auth/login.tsx
+++ Keepdishing/react-app/pages/auth/login.tsx
@@ -8,6 +8,7 @@ import {
   FormControl,
   Input,
   Image,
+  Text,
   Button,
   Switch,
   Container,
@@ -58,43 +59,54 @@ const Login = () => {
   return (
     <Container pt={[10, 20]}>
       <Center w="full" h="full" flexDirection="column">
-        <Image w={["80%", "sm"]} mb={8} src="/logo.svg" alt="logo" />
+        <Image w={"xs"} mb={8} src="/logo.svg" alt="logo" />
         <Box bg="white" p={12} borderRadius="md" boxShadow="xl">
-          <Heading mb={8}>Login</Heading>
-          <QueryErrorAlert error={error.error} />
-          <Box w="full">
-            <form onSubmit={onSubmit}>
-              <VStack spacing={4}>
-                <FormControl isInvalid={errors.email != null}>
-                  <FormLabel>Email</FormLabel>
-                  <Input {...register("email")} autoComplete="email" />
-                  <FormErrorMessage>{errors.email && errors.email.message}</FormErrorMessage>
-                </FormControl>
-                <FormControl isInvalid={errors.password != null}>
-                  <FormLabel>Password</FormLabel>
-                  <PasswordField {...register("password")} autoComplete="password" />
-                  <FormErrorMessage>{errors.password && errors.password.message}</FormErrorMessage>
-                </FormControl>
-                <Flex w="full" justifyContent="space-between">
-                  <FormControl display="flex" alignItems="center">
-                    <FormLabel htmlFor="rememberMe" mb="0">
-                      Remember Me
-                    </FormLabel>
-                    <Switch colorScheme="blue" defaultChecked {...register("rememberMe")} id="rememberMe" />
+          <VStack spacing={8}>
+            <Heading>Login</Heading>
+            <QueryErrorAlert error={error.error} />
+            <Box w="full">
+              <form onSubmit={onSubmit}>
+                <VStack spacing={4}>
+                  <FormControl isInvalid={errors.email != null}>
+                    <FormLabel>Email</FormLabel>
+                    <Input {...register("email")} autoComplete="email" />
+                    <FormErrorMessage>{errors.email && errors.email.message}</FormErrorMessage>
                   </FormControl>
-                  <NextLink href="/auth/signup" passHref>
-                    <Link textAlign="end" w="full" fontWeight="bold" color="brand.link">
-                      Forgot Password
+                  <FormControl isInvalid={errors.password != null}>
+                    <FormLabel>Password</FormLabel>
+                    <PasswordField {...register("password")} autoComplete="password" />
+                    <FormErrorMessage>{errors.password && errors.password.message}</FormErrorMessage>
+                  </FormControl>
+                  <Flex w="full" justifyContent="space-between">
+                    <FormControl display="flex" alignItems="center">
+                      <FormLabel htmlFor="rememberMe" mb="0">
+                        Remember Me
+                      </FormLabel>
+                      <Switch colorScheme="blue" defaultChecked {...register("rememberMe")} id="rememberMe" />
+                    </FormControl>
+                    <NextLink href="/auth/forgot-password" passHref>
+                      <Link textAlign="end" w="full" fontWeight="semibold" color="brand.link">
+                        Forgot Password
+                      </Link>
+                    </NextLink>
+                  </Flex>
+                </VStack>
+                <Button my={10} w="full" isLoading={isSubmitting} type="submit">
+                  Log in
+                </Button>
+              </form>
+              <Center>
+                <Text>
+                  Need an Account?{" "}
+                  <NextLink href="/auth/forgot-password" passHref>
+                    <Link ml="1" textAlign="end" w="full" fontWeight="semibold" color="brand.link">
+                      Sign Up
                     </Link>
                   </NextLink>
-                </Flex>
-              </VStack>
-
-              <Button w="full" mt={10} isLoading={isSubmitting} type="submit">
-                Log in
-              </Button>
-            </form>
-          </Box>
+                </Text>
+              </Center>
+            </Box>
+          </VStack>
         </Box>
       </Center>
     </Container>
